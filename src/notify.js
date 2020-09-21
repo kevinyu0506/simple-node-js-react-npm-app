e133e5dc385c03954169abe76d874f4a1031ddf4
Kevin Yu <kevinyu@TW-LW60362-M.local>
2020-09-21 18:42:01 +0800

    Remove semicolon

--- src/notify.js
+++ src/notify.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
-const qs = require('qs');
+const axios = require('axios')
+const qs = require('qs')
 
 if (process.argv.length !== 6) {
-    console.log("Usage: node notify.js <token> <description> $JOB_NAME $BUILD_URL");
-    throw "stop execution";
+    console.log("Usage: node notify.js <token> <description> $JOB_NAME $BUILD_URL")
+    throw "stop execution"
 }
 
 const token = process.argv[2]
-const data = { 'message': `${process.argv[3]}\nJob: ${process.argv[4]}\nUrl: ${process.argv[5]}` };
+const data = { 'message': `${process.argv[3]}\nJob: ${process.argv[4]}\nUrl: ${process.argv[5]}` }
 
 axios({
     method: 'post',
@@ -21,4 +21,4 @@ axios({
     console.log(res);
 }).catch(function(err) {
     console.log(err);
-});
+})

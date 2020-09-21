61cc658dea1d635a1923c08930bfdb7fb2cc1d31
Kevin Yu <kevinyu@TW-LW60362-M.local>
2020-09-21 18:22:26 +0800

    Update notify.js

--- src/notify.js
+++ src/notify.js
@@ -1,18 +1,24 @@
 const axios = require('axios');
 const qs = require('qs');
 
-const data = { 'message': process.argv.slice(2)[0] };
+if (process.argv.length !== 6) {
+    console.log("Usage: node notify.js <token> <description> $JOB_NAME $BUILD_URL");
+    throw "stop execution";
+}
+
+const token = process.argv[2]
+const data = { 'message': `${process.argv[3]}\nJob: ${process.argv[4]}\nUrl: ${process.argv[5]}` };
 
 axios({
     method: 'post',
     url: 'https://notify-api.line.me/api/notify',
     data: qs.stringify(data),
     headers: {
-        'Authorization': 'Bearer zgCGMfH7XzqIa0mDhiQ257siaP4PHxaV3xdbmH6dA4r',
+        'Authorization': `Bearer ${token}`,
         'content-type': 'application/x-www-form-urlencoded'
     }
-}).then(function(response) {
-    console.log(response);
-}).catch(function(error) {
-    console.log(error);
+}).then(function(res) {
+    console.log(res);
+}).catch(function(err) {
+    console.log(err);
 });
